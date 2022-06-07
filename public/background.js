f539b8527c2435575301a1c296c11581a5e9eb76
Zen <muhzaini30@gmail.com>
2022-06-08 06:54:55 +0800

    Edit manifest sama background ke format Firefox

--- public/background.js
+++ public/background.js
@@ -1,14 +1,14 @@
-chrome.runtime.onInstalled.addListener(({ reason, version }) => {
-  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+browser.runtime.onInstalled.addListener(({ reason, version }) => {
+  if (reason === browser.runtime.OnInstalledReason.INSTALL) {
     showReadme();
   }
 });
 
-chrome.action.onClicked.addListener((tab) => {
+browser.browserAction.onClicked.addListener((tab) => {
   showReadme();
 });
 
 function showReadme(info, tab) {
-  let url = chrome.runtime.getURL("index.html");
-  chrome.tabs.create({ url });
+  let url = browser.runtime.getURL("index.html");
+  browser.tabs.create({ url });
 }
